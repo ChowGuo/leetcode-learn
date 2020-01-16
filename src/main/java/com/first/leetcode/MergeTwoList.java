3e2f2b4c22e7db1f41ea4409436aa345530bc08d
Lynn <lynnguo42@outlook.com>
2020-01-16 14:49:52 +0800

    合并两个有序列表

--- src/main/java/com/first/leetcode/MergeTwoList.java
+++ src/main/java/com/first/leetcode/MergeTwoList.java
@@ -22,8 +22,27 @@ public class MergeTwoList {
             res = res.next;
         }
     }
+
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null) {
+        ListNode res = new ListNode(0);
+        ListNode cur = res;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = new ListNode(l1.val);
+                l1 = l1.next;
+            } else {
+                cur.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 == null ? l2 : l1;
+
+        return res.next;
+
+
+
+        /*if(l1 == null) {
             return l2;
         }
         if(l2 == null) {
@@ -36,7 +55,7 @@ public class MergeTwoList {
         } else {
             l2.next = mergeTwoLists(l1, l2.next);
             return l2;
-        }
+        }*/
     }
 }
 
